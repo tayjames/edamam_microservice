bded9120e3464015a8dfd853d49aa32f2a3da721
Tay <tay.james.d@gmail.com>
2019-10-12 13:07:54 -0600

    Creates route for recipes, including the recipesRouter var. Also requires dotenv package

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var recipesRouter = require('./routes/api/v1/recipes');
 
 var app = express();
 
@@ -16,5 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/v1/recipes', recipesRouter)
 
 module.exports = app;
