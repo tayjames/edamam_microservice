c414f4d34076f8af82b889caa523acfdce9f4b2f
Tay <tay.james.d@gmail.com>
2019-10-14 16:22:52 -0600

    Food search WIP

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -4,7 +4,6 @@ var fetch = require('node-fetch');
 var Recipe = require('../../../models').Recipe;
 
 router.get('/', function(req, res, next) {
-
   Recipe.findAll()
     .then(recipes => {
       res.setHeader("Content-Type", "application/json");
@@ -14,6 +13,56 @@ router.get('/', function(req, res, next) {
       res.setHeader("Content-Type", "application/json");
       res.status(500).send({error})
     })
+});
+
+router.get('/food_search', function(req, res, next) {
+  Recipe.findAll({
+    where: {
+      foodType: req.query.q
+    }
+  })
+    .then(list => {
+      if (list[0] == undefined) {
+        let edamam = `https://api.edamam.com/search?q=${req.query.q}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_APP_KEY}`
+        fetch(edamam)
+        .then(response => response.json())
+        .then(result => result["hits"])
+        .then(recipes => {
+          var recipeData = []
+          recipes.forEach(function(element) {
+            var receta = {
+              foodType: req.query.q,
+              recipe: element.recipe.label,
+              recipeUrl: element.recipe.url,
+              servings: element.recipe.yield,
+              ingredients: element.recipe.ingredients.length,
+              calories: element.recipe.calories,
+              prepTime: element.recipe.totalTime
+            }
+            recipeData.push(receta)
+          })
+            return recipeData
+          })
+          .then(recipeData => {
+            Recipe.bulkCreate(recipeData)
+        })
+          .then(createdRecipes => {
+            res.setHeader("Content-Type", "application/json");
+            res.status(201).send(JSON.stringify(createdRecipes))
+          })
+        .catch(error => {
+          res.setHeader("Content-Type", "application/json");
+          res.status(500).send({error});
+        })
+      } else {
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).send(JSON.stringify(list));
+      }
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({error})
+    });
 });
 
 module.exports = router;
--- spec/requests/api/v1/recipes.spec.js
+++ spec/requests/api/v1/recipes.spec.js
@@ -2,9 +2,7 @@ var shell = require('shelljs');
 var request = require('supertest');
 var app = require('../../../../app');
 const fetch = require('node-fetch');
-var Recipe = require('../../../models').Recipe;
-
-
+var Recipe = require('../../../../models').Recipe;
 
 describe('Edamam API', () => {
   beforeAll(() => {
@@ -18,11 +16,28 @@ describe('Edamam API', () => {
     shell.exec('npx sequelize db:migrate:undo:all')
   });
 
-  test('GET /api/v1/recipes', async () => {
-    return reqest(app)
+  test('GET /api/v1/recipes', () => {
+    return request(app)
     .get('/api/v1/recipes')
     .then(response => {
       expect(response.statusCode).toBe(200);
     })
   })
+
+  test('GET /api/v1/recipes/food_search?q=___ (stored in database)', async () => {
+    return request(app)
+    .get('api/v1/recipes/food_search?q=Pineapple')
+    .then(response => {
+      console.log(response.statusCode)
+      expect(response.statusCode).toBe(200),
+      expect(response.body.length).toBe(5),
+      expect(response.body[0]).toContain("foodType"),
+      expect(response.body[0]).toContain("recipe"),
+      expect(response.body[0]).toContain("recipeUrl"),
+      expect(response.body[0]).toContain("servings"),
+      expect(response.body[0]).toContain("ingredients"),
+      expect(response.body[0]).toContain("calories"),
+      expect(response.body[0]).toContain("prepTime")
+    })
+  } )
 })
