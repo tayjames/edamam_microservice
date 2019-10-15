99d5638e6b7088ec9b5d733945daec70047e5913
Tay <tay.james.d@gmail.com>
2019-10-15 17:38:07 -0600

    Creates calorie_search route so that a user is able to search for foods within a specified range of calories

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var fetch = require('node-fetch');
 var Recipe = require('../../../models').Recipe;
+var Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 router.get('/', function(req, res, next) {
 
@@ -14,6 +16,29 @@ router.get('/', function(req, res, next) {
       res.setHeader("Content-Type", "application/json");
       res.status(500).send({error})
     })
+});
+
+router.get('/calorie_search', function(req, res, next) {
+  Recipe.findAll ({
+    where: {
+      calories: {
+        [Op.between]: [req.query.q.split('-')[0], req.query.q.split('-')[1]]
+      }
+    }
+  })
+  .then(calories => {
+    if (req.query.q.split('-').length == 2) {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(calories))
+  } else {
+      res.setHeader("Content-Type", "application/json");
+      res.status(400).send(JSON.stringify("Be sure to send this request as a range separated with a '-'"))
+  }
+  })
+  .catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader(500).send({error})
+  });
 });
 
 module.exports = router;
