87f215ba4ec03ab12c4306a36e8b9c105bd0d9bc
Tay <tay.james.d@gmail.com>
2019-10-15 16:55:40 -0600

    Creates basic testing that covers food_search endpoint -- WIP

--- spec/requests/api/v1/recipes.spec.js
+++ spec/requests/api/v1/recipes.spec.js
@@ -26,18 +26,42 @@ describe('Edamam API', () => {
 
   test('GET /api/v1/recipes/food_search?q=___ (stored in database)', async () => {
     return request(app)
-    .get('api/v1/recipes/food_search?q=Pineapple')
+    .get('/api/v1/recipes/food_search?q=Pineapple')
     .then(response => {
       console.log(response.statusCode)
       expect(response.statusCode).toBe(200),
       expect(response.body.length).toBe(5),
-      expect(response.body[0]).toContain("foodType"),
-      expect(response.body[0]).toContain("recipe"),
-      expect(response.body[0]).toContain("recipeUrl"),
-      expect(response.body[0]).toContain("servings"),
-      expect(response.body[0]).toContain("ingredients"),
-      expect(response.body[0]).toContain("calories"),
-      expect(response.body[0]).toContain("prepTime")
+      expect(Object.keys(response.body[0])).toContain("foodType"),
+      expect(Object.keys(response.body[0])).toContain("recipe"),
+      expect(Object.keys(response.body[0])).toContain("recipeUrl"),
+      expect(Object.keys(response.body[0])).toContain("servings"),
+      expect(Object.keys(response.body[0])).toContain("ingredients"),
+      expect(Object.keys(response.body[0])).toContain("calories"),
+      expect(Object.keys(response.body[0])).toContain("prepTime")
     })
   } )
+
+  test('Returns 500 status code when no query is sent to GET /api/v1/recipes/food_search?q=___', async () => {
+    return request(app)
+    .get('/api/v1/recipes/food_search?')
+    .then(response => {
+      expect(response.statusCode).toBe(500)
+    })
+  } )
+
+  // test('GET /api/v1/recipes/food_search?q=___ (not stored in database)', async () => {
+  //   return request(app)
+  //   .get('/api/v1/recipes/food_search?q=artichoke')
+  //   .then(response => {
+  //     expect(response.statusCode).toBe(200),
+  //     expect(response.body.length).toBe(10),
+  //     expect(Object.keys(response.body[0])).toContain("foodType"),
+  //     expect(Object.keys(response.body[0])).toContain("recipe"),
+  //     expect(Object.keys(response.body[0])).toContain("recipeUrl"),
+  //     expect(Object.keys(response.body[0])).toContain("servings"),
+  //     expect(Object.keys(response.body[0])).toContain("ingredients"),
+  //     expect(Object.keys(response.body[0])).toContain("calories"),
+  //     expect(Object.keys(response.body[0])).toContain("prepTime")
+  //   })
+  // } )
 })
