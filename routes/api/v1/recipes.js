4d090a31c3628928f627411cf06ada105aef35e8
Tay <tay.james.d@gmail.com>
2019-10-15 21:53:53 -0600

    Creates route that allows a user to find the average calories of a food type across 10 different recipes

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -173,5 +173,65 @@ router.get('/sort_ingredients', function(req, res, next) {
     });
 })
 
+router.get('/average_calories', function(req, res, next) {
+  Recipe.findAll({
+    where: {
+      foodType: req.query.q
+    },
+    attributes: [
+      [Sequelize.fn('AVG', Sequelize.col('calories')), 'averageCalories']
+    ]
+  })
+    .then(list => {
+      if (list.attributes == undefined) {
+        let edamam = `https://api.edamam.com/search?q=${req.query.q}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_APP_KEY}`
+        fetch(edamam)
+        .then(res => res.json())
+        .then(result => result["hits"])
+        .then(recipes => {
+          var recipeData = []
+          recipes.forEach(function(element) {
+            var receta = {
+              foodType: req.query.q,
+              recipe: element.recipe.label,
+              recipeUrl: element.recipe.url,
+              servings: element.recipe.yield,
+              ingredients: element.recipe.ingredients.length,
+              calories: element.recipe.calories,
+              prepTime: element.recipe.totalTime
+            }
+            recipeData.push(receta)
+          })
+            return Recipe.bulkCreate(recipeData)
+          })
+          .then(createdRecipes => {
+            return Recipe.findAll({
+              where: {
+                foodType: req.query.q
+              },
+              attributes: [
+                [Sequelize.fn('AVG', Sequelize.col('calories')), 'averageCalories']
+              ]
+            })
+          })
+          .then(averageCalories => {
+            res.setHeader("Content-Type", "application/json");
+            res.status(201).send(JSON.stringify(averageCalories))
+          })
+        .catch(error => {
+          res.setHeader("Content-Type", "application/json");
+          res.status(500).send({error});
+        })
+      } else {
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).send(JSON.stringify(list));
+      }
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({error})
+    });
+})
+
 
 module.exports = router;
